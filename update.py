24370d43ce3b4a239abf6c4c7b88ad4d0b6f115e
KID <minhthong.it2015@gmail.com>
2017-11-09 09:33:03 +0700

    improve update

--- update.py
+++ update.py
@@ -6,6 +6,7 @@ import threading
 import time
 from subprocess import call
 import argparse
+from distutils.version import LooseVersion, StrictVersion
 
 def cmd(command):
   call(command.split(' '))
@@ -17,10 +18,10 @@ class Updater:
     
     try:
       f = open('version.ini', 'r+')
-      self.cur_version = f.read()
+      self.cur_version = LooseVersion(f.read())
       print("[SYS] > Current firmware version: {}".format(self.cur_version))
     except:
-      self.cur_version = ''
+      self.cur_version = LooseVersion('')
       pass
   
   def keep_up_date(self):
@@ -36,27 +37,25 @@ class Updater:
   def resolveResult(self, result):
     lines = result.split('\r\n')
     if len(lines) <= 0: return
-    new_version = lines[0]
-    if new_version == self.cur_version: return
-
-    i_new_version = new_version.split('.')
-    i_cur_version = self.cur_version.split('.')
-    for (new, cur) in zip(i_new_version, i_cur_version):
-      if new < cur: return
-
-    print("[SYS] > Newer firmware found. Start installing newer version!")
-    self.hard_update(new_version)
+    new_version = LooseVersion(lines[0])
+    if new_version <= self.cur_version: return
+    else:
+      print("[SYS] > Newer firmware found. Start installing newer version!")
+      print("[SYS] > Update from {} to {}".format(self.cur_version, new_version))
+      self.hard_update(new_version)
 
   def hard_update(self, new_version=''):
     cmd('git fetch --all')
     cmd('git reset --hard origin/master')
+    
+    fnew = open('version.ini', 'w')
+    fnew.write(new_version if new_version != '' else self.cur_version)
+    fnew.close()
+    
     cmd('sudo chmod +x ./startup')
     cmd('sudo systemctl restart insys')
     cmd('sudo chmod +x ./update.py')
     cmd('sudo systemctl restart insys_update')
-    fnew = open('version.ini', 'w')
-    fnew.write(new_version if new_version != '' else self.cur_version)
-    fnew.close()
 
 if __name__ == "__main__":
   insys_updater = Updater("https://drive.google.com/uc?export=download&id=0B-_M0TAaeEKgY1ZjQ2lXQ2I5dkk", 30)
