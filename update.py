3ac8429291c296161c3be7a0a293968bf4534684
KID <minhthong.it2015@gmail.com>
2017-11-09 08:56:30 +0700

    update with reset option

--- update.py
+++ update.py
@@ -5,7 +5,10 @@ import datetime
 import threading
 import time
 from subprocess import call
+import argparse
 
+def cmd(command):
+  call(command.split(' '))
 
 class Updater:
   def __init__(self, update_url, refresh_time=10):
@@ -14,10 +17,10 @@ class Updater:
     
     try:
       f = open('version.ini', 'r+')
-      self.curversion = f.read()
-      print("[SYS] > Current firmware version: {}".format(self.curversion))
+      self.cur_version = f.read()
+      print("[SYS] > Current firmware version: {}".format(self.cur_version))
     except:
-      self.curversion = ''
+      self.cur_version = ''
       pass
   
   def keep_up_date(self):
@@ -38,15 +41,26 @@ class Updater:
     lines = result.split('\r\n')
     if len(lines) <= 0: return
     new_version = lines[0]
-    if new_version <= self.curversion: return
+    if new_version <= self.cur_version: return
 
     print("[SYS] > Newer firmware found. Start installing newer version!")
+    self.hard_update()
 
-    call('git pull'.split(' '))
-    call('sudo systemctl restart insys'.split(' '))
+  def hard_update(self, new_version=''):
+    cmd('git fetch --all')
+    cmd('git reset --hard origin/master')
+    cmd('sudo systemctl restart insys')
     fnew = open('version.ini', 'w')
-    fnew.write(new_version)
+    fnew.write(new_version if new_version != '' else self.cur_version)
 
 if __name__ == "__main__":
   insys_updater = Updater("https://drive.google.com/uc?export=download&id=0B-_M0TAaeEKgY1ZjQ2lXQ2I5dkk", 30)
-  insys_updater.keep_up_date()
+
+  parser = argparse.ArgumentParser(description='Update InSys firmware')
+  parser.add_argument('-r', action='store_true', help='Hard update current firmware')
+
+  args = parser.parse_args(['-h'])
+  if args['r']:
+    insys_updater.hard_update()
+  else:
+    insys_updater.keep_up_date()
